d9d7f3f7f2648431a6e6945c4384b39e3a6f4f90
doterlm <595929957@qq.com>
2018-04-17 15:30:19 +0800

    uwsgi

---
+++ gunicorn.py
@@ -0,0 +1,13 @@
+bind='127.0.0.1:8001'
+
+backlog=2048
+workers=2
+worker_connections=100
+worker_class='sanic.worker.GunicornWorker'
+timeout=30
+keepalive=2
+chdir='/root/sanicapi'
+
+spew=False
+daemon=False
+umask=0
