65d549794133c6746f26499c93db73c84381011d
doterlm <595929957@qq.com>
2018-05-11 09:57:48 +0800

    uwsgi

--- api_1/qiniu/qiniuapi.py
+++ api_1/qiniu/qiniuapi.py
@@ -1,39 +1,24 @@
-import asyncpg
-
 from sanic import response
 
 from api_1.__init__  import api_1
-from qiniu.services.cdn.manager import create_timestamp_anti_leech_url
-import time
-
-#
-#
-# # @api_1.route('/qiniu')
-# host='http://p8aitf97j.bkt.clouddn.com'
-# encrypt_key=''
-filename = '一生所爱_纯音频文件_纯音频输出.MP3'
-# query_string=''
-#
-# deadline=int(time.time())+3600
-# timestamp_url=create_timestamp_anti_leech_url(host,filename,query_string,encrypt_key,deadline)
-# print(timestamp_url)
-import requests
 from qiniu import Auth
 
 
-# access_key='KyyJ2b9IhoNBKtRNLaKphZ0SfNxI0g9pKedgGNPo'
-# secret_key='HXQ8JNTnwoYeRb1AMDctYpEzhdKZzF9LOPiv-A2U'
-# q=Auth(access_key,secret_key)
-# bucket_domain='p8aitf97j.bkt.clouddn.com'
-# base_url='http://%s/%s'%(bucket_domain,filename)
-# private_url=q.private_download_url(base_url,expires=3600)
-# print(private_url)
-#
-# r=requests.get(private_url)
-# assert r.status_code==200
+access_key='KyyJ2b9IhoNBKtRNLaKphZ0SfNxI0g9pKedgGNPo'
+secret_key='HXQ8JNTnwoYeRb1AMDctYpEzhdKZzF9LOPiv-A2U'
+q=Auth(access_key,secret_key)
+bucket_domain='p8aitf97j.bkt.clouddn.com'
 
-@api_1.route('/qiniudown/<tag1>/<tag2>')
-async def qiniudown(request,tag1,tag2):
-    print(request)
 
-    return response.json({'a': format(tag1),'b': format(tag2)}, 200)
+# 获取七牛资源链接
+@api_1.route('/qiniudown/<tag1>')
+async def qiniudown(request,tag1):
+    filename = tag1+'.MP3'
+    base_url = 'http://%s/%s' % (bucket_domain, filename)
+    private_url=q.private_download_url(base_url,expires=3600)
+    return response.json({'a': format(private_url)}, 200)
+
+# 上传歌曲
+# @api_1.route('/qiniuup/<>')
+
+
--- run.py
+++ run.py
@@ -6,4 +6,4 @@ from api_1.__init__ import api_1
 app=Sanic(__name__)
 app.blueprint(api_1,url_prefix='v1')
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-app.run(host='0.0.0.0',port=8000,debug=True)
+# app.run(host='0.0.0.0',port=8000,debug=True)
